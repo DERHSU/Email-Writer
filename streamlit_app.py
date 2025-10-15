b9016d7548e1458f62536d88bc7fab76bf916397
DERHSU <derrickh91789@gmail.com>
2025-10-15 02:04:29 +0000

    I used streamlit platform

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,37 +2,24 @@ import streamlit as st
 # 1
 from openai import OpenAI
 
-# 2
 client = OpenAI(
-    api_key = "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"
+    api_key = "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"
 )
-print("Beep-Boop I write good emails Beep-Boop")
-name = input("What is your name? -> ")
-recipient = input("Who are you sending this to? -> ")
-valid = False
-while not valid:
-    print("Pick from these writing styles: ")
-    print("1. Sad")
-    print("2. Energetic")
-    print("3. Apologetic")
-    print("4. Comedic")
-    print("5. Formal")
-    style = input("Which style would you like? -> ") 
-    valid = True
-    if style == "1":
-        style = "sad"
-    elif style == "2":
-        style = "energetic"
-    elif style == "3":
-        style = "apologetic"
-    elif style == "4":
-        style = "comedic"
-    elif style == "5":
-        style = "formal"
-    else:
-        print("That is not a valid entry, please try again.")
-        valid = False
-content = input("What would you like this email to be about? -> ")
+name = st.text_input("What is your name?")
+recipient = st.text_input("Who are you sending this to?")
+style = st.selectbox(
+    "Which style would you like?",
+    [
+        "Sad",
+        "Energetic",
+        "Apologetic",
+        "Comedic",
+        "Formal"
+    ]
+)
+
+content = st.text_area("What would you like this email to be about?")
+run = st.button("Submit your request")
 # 3
 content_message = """
 Follow standard email format.
@@ -43,21 +30,23 @@ Subject: Grades
 Message: You are cool
 """
 assistant_message = "You're supposed to write the best emails possible"
-user_prompt = f"Can you write me an email when the writer's name is {name} and the recipient's name is {recipient}. Make sure the email is about {content} and the email style is {style}."
-print(user_prompt)
-# 4
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo-0125",
-    messages=[
-        {"role": "system", "content": content_message},
-        {"role": "assistant", "content": assistant_message},
-        {"role": "user", "content": user_prompt}
-    ]
-)
-
+if run:
+    user_prompt = f"Can you write me an email when the writer's name is {name} and the recipient's name is {recipient}. Make sure the email is about {content} and the email style is {style}."
+    # 4
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": content_message},
+            {"role": "assistant", "content": assistant_message},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+    st.write(response.choices[0].message.content)
 # 5
-print(response.choices[0].message.content)
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
